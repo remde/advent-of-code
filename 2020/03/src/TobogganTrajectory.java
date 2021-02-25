9d2e0f12958105d143fff241f4f1cf54eacc1fab
remde <ffremde@gmail.com>
2021-02-25 18:57:49 -0300

    Add 03 structure

---
+++ 2020/03/src/TobogganTrajectory.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TobogganTrajectory {
+
+    static List<String> treeMap = new ArrayList<>();
+
+    public static void main(String[] args) {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(
+                    "../input.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                treeMap.add(line);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.print(treeMap);
+    }
+
+}
