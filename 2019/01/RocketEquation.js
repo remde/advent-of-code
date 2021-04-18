b29ac5517425675b40ee8e514079b948d40a5171
remde <ffremde@gmail.com>
2021-04-18 14:59:43 -0300

    Refactor 2019 01

--- 2019/01/RocketEquation.js
+++ 2019/01/RocketEquation.js
@@ -3,20 +3,20 @@ const input = fs.readFileSync('input.txt', 'utf-8')
                 .trim()
                 .split('\n')
 
-// Part 1
 const getFuelFromMass = (mass) => Math.floor(parseInt(mass)/3) - 2;
 
-const totalFuel = input.map(mass => getFuelFromMass(mass))
-                  .reduce((a, b)=> a + b)
-console.log(totalFuel)
-
-// Part 2
 const getModularFuel = (mass) => {
     let fuel = getFuelFromMass(mass)
     if (fuel > 0) return getModularFuel(fuel) + fuel
     return 0
 }
 
+// Part 1
+const totalFuel = input.map(mass => getFuelFromMass(mass))
+                  .reduce((a, b)=> a + b)
+console.log(totalFuel)
+
+// Part 2
 const totalModularFuel = input.map(mass => getModularFuel(mass))
                        .reduce((a, b) => a + b)
 console.log(totalModularFuel)
